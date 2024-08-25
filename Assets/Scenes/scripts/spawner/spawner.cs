6b002ded6ac8fc16917389b346e34c257526bb78
Angelina Fetter <angiefetter25@gmail.com>
2024-08-25 20:12:12 -0300

    modifique script spawner

--- Assets/Scenes/scripts/spawner/spawner.cs
+++ Assets/Scenes/scripts/spawner/spawner.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private float timeToSpawn;
+
+    private float totalTime;
+    private bool spawned = false;
+    private GameObject obstacle;
+    private GameObject obstacle2;
+    
 
 
     void Start()
     {
-        Instantiate(obstaclePrefab, new Vector2(0, 0), Quaternion.identity);
+        totalTime = 0;
     }
 
    
     void Update()
     {
-        
+        totalTime += Time.deltaTime;
+        if (totalTime > timeToSpawn && !spawned)
+        {
+            float randomobs = Random.value;
+            float randomobs2 = Random.value;
+
+            obstacle = Instantiate(obstaclePrefab, new Vector2(randomobs, randomobs2), Quaternion.identity);
+            obstacle2 = Instantiate(obstaclePrefab, new Vector2(randomobs, randomobs2), Quaternion.identity);
+            spawned = true;
+        }
+
+
+        if (totalTime > 6)
+        {
+            obstacle.transform.position += new Vector3(0, 0, 40);
+            obstacle2.transform.position += new Vector3(0, 0, 40);
+        }
+
     }
 }
